76aa60bbfd911c7c6d977b26e10bb080c1c37b20
JulianRC1 <juldarecar2003@gmail.com>
2025-04-22 19:59:06 -0500

    fix: update background color and padding in Screen component

--- components/Screen.jsx
+++ components/Screen.jsx
@@ -1,5 +1,5 @@
 import { View } from "react-native";
 
 export function Screen({ children }) {
-  return <View className="flex-1 bg-purple-200 pt-4 px-4">{children}</View>;
+  return <View className="flex-1 bg-[#E0BBE4] pt-8">{children}</View>;
 }
