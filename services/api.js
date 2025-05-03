a736e578caca82c1c4ee0e5f380cf88283b3eac8
juanloaiza007 <juandaloaiza723@gmail.com>
2025-05-03 00:27:25 -0500

    fix(api): implement API injector for token and header management

--- services/api.js
+++ services/api.js
@@ -1,5 +1,7 @@
+// services/api.js
 import axios from "axios";
 import Constants from "expo-constants";
+import { getToken } from "../utils/asyncStorage";
 
 const API_URL = Constants.expoConfig.extra.API_URL;
 
@@ -10,3 +12,24 @@ export const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    const needsAuth = !config.url.includes("/usuario/");
+
+    if (needsAuth) {
+      const token = await getToken();
+      if (token) {
+        config.headers.Authorization = `${token}`; // Inyecta el token, NO es necesario el "Bearer"
+      }
+      console.log("Verification header added");
+    } else {
+      delete config.headers;
+      console.log("Headers cleaned");
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error); // Retorna error si por ejemplo el token no es válido
+  }
+);
