e56a6a2430fc074afc2bee57695c517270e5a7b2
JulianRC1 <juldarecar2003@gmail.com>
2025-05-05 22:27:13 -0500

    refactor: remove unused login and registration hooks and related auth service functions

--- services/auth.js
+++
@@ -1,8 +0,0 @@
-import { api } from "./api";
-
-// POST /usuario/login
-export const fetchlogin = (payload) => api.post("/usuario/login", payload);
-
-// POST /usuario/registro
-export const fetchRegister = (payload) =>
-  api.post("/usuario/registrar", payload);
