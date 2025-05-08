c3c1ea1d6d14b93929b3a25ee6d8f1bc5155ff48
JulianRC1 <juldarecar2003@gmail.com>
2025-05-08 00:54:32 -0500

    feat: enhance ProfilePage with logout functionality and improved layout

--- app/(tabs)/admin/profile.js
+++ app/(tabs)/admin/profile.js
@@ -1,11 +1,30 @@
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, TouchableOpacity } from "react-native";
 import { Screen } from "../../../components/Screen";
+import { useAuth } from "../../../context/authContext";
+import { useRouter } from "expo-router";
 
 export default function ProfilePage() {
+  const { logout } = useAuth();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await logout();
+    router.replace("/login");
+  };
+
   return (
     <Screen>
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ padding: 16 }}>
         <Text className="text-black text-xl font-bold mb-6">Perfil</Text>
+
+        <TouchableOpacity
+          onPress={handleLogout}
+          className="bg-purple-600 py-3 px-4 rounded-md"
+        >
+          <Text className="text-white text-center font-semibold">
+            Cerrar sesión
+          </Text>
+        </TouchableOpacity>
       </ScrollView>
     </Screen>
   );
