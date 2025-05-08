1119d45dd8bb4298130116fe93d9972b2555a058
JulianRC1 <juldarecar2003@gmail.com>
2025-05-08 00:05:33 -0500

    fix: update API endpoint URLs for consistency and add logging in fetchSedes

--- services/locations.js
+++ services/locations.js
@@ -1,17 +1,22 @@
+// services/locations.js
 import { api } from "./api";
 
-//GET /usuario/sedes
-export const fetchSedes = () => api.get("/usuario/sedes");
+// ===== USER =====
 
-//GET /admin/sedes
-export const fetchSedesAdmin = () => api.get("/admin/sedes");
+// GET /usuario/servicios
+export const fetchSedes = () => {
+  console.log("Configuracion de la peticion", api.defaults);
+  return api.get("/usuario/sedes/");
+};
 
-//POST /admin/sedes
-export const createSede = (payload) => api.post("/admin/sedes", payload);
+// ===== ADMIN =====
 
-//PUT /admin/sedes/:id
+// POST /admin/sedes
+export const createSede = (payload) => api.post("/admin/sedes/", payload);
+
+// PUT /admin/sedes/:id
 export const updateSede = (id, payload) =>
-  api.put(`/admin/sedes/${id}`, payload);
+  api.put(`/admin/sedes/${id}/`, payload);
 
-//DELETE /admin/sedes/:id
-export const deleteSede = (id) => api.delete(`/admin/sedes/${id}`);
+// DELETE /admin/sedes/:id
+export const deleteSede = (id) => api.delete(`/admin/sedes/${id}/`);
