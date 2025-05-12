3288e2f934a934416a8e899e4f2f8cabd15e964b
juanloaiza007 <juandaloaiza723@gmail.com>
2025-05-12 18:38:51 -0500

    feat(admin): add mock data for linked services - SASPA-105

---
+++ services/mocks/teamServicesMocks.js
@@ -0,0 +1,6 @@
+export const serviciosVinculadosMock = [
+  { empleadoId: 4, servicioId: 1 },
+  { empleadoId: 4, servicioId: 3 },
+  { empleadoId: 4, servicioId: 2 },
+  { empleadoId: 3, servicioId: 1 },
+];
